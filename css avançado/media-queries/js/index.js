156b3c0587ec1072b6f7b13cec41f5823c597267
Francisco <franciscosensaulas@gmail.com>
2025-02-14 22:09:26 -0300

    Finalizado exemplo de animations e iniciado media-queries

---
+++ css avançado/media-queries/js/index.js
@@ -0,0 +1,182 @@
+const cars = [
+    {
+        model: "Volkswagen Gol",
+        year: 2022,
+        price: 75000,
+        mileage: 15000,
+        fuel: "Flex",
+        brand: "volkswagen",
+        image: "https://picsum.photos/300/200?car=1"
+    },
+    {
+        model: "Fiat Argo",
+        year: 2020,
+        price: 68000,
+        mileage: 25000,
+        fuel: "Gasolina",
+        brand: "fiat",
+        image: "https://picsum.photos/300/200?car=2"
+    },
+    {
+        model: "Ford Ka",
+        year: 2019,
+        price: 45000,
+        mileage: 40000,
+        fuel: "Flex",
+        brand: "ford",
+        image: "https://picsum.photos/300/200?car=3"
+    },
+    {
+        model: "Chevrolet Onix",
+        year: 2021,
+        price: 82000,
+        mileage: 18000,
+        fuel: "Flex",
+        brand: "chevrolet",
+        image: "https://picsum.photos/300/200?car=4"
+    },
+    {
+        model: "Volkswagen T-Cross",
+        year: 2023,
+        price: 135000,
+        mileage: 5000,
+        fuel: "Gasolina",
+        brand: "volkswagen",
+        image: "https://picsum.photos/300/200?car=5"
+    },
+    {
+        model: "Fiat Toro",
+        year: 2022,
+        price: 145000,
+        mileage: 12000,
+        fuel: "Diesel",
+        brand: "fiat",
+        image: "https://picsum.photos/300/200?car=6"
+    },
+    {
+        model: "Ford Ranger",
+        year: 2020,
+        price: 185000,
+        mileage: 35000,
+        fuel: "Diesel",
+        brand: "ford",
+        image: "https://picsum.photos/300/200?car=7"
+    },
+    {
+        model: "Chevrolet Cruze",
+        year: 2021,
+        price: 110000,
+        mileage: 22000,
+        fuel: "Flex",
+        brand: "chevrolet",
+        image: "https://picsum.photos/300/200?car=8"
+    },
+    {
+        model: "Volkswagen Polo",
+        year: 2019,
+        price: 65000,
+        mileage: 45000,
+        fuel: "Flex",
+        brand: "volkswagen",
+        image: "https://picsum.photos/300/200?car=9"
+    },
+    {
+        model: "Fiat Mobi",
+        year: 2023,
+        price: 55000,
+        mileage: 8000,
+        fuel: "Gasolina",
+        brand: "fiat",
+        image: "https://picsum.photos/300/200?car=10"
+    },
+    {
+        model: "Ford EcoSport",
+        year: 2018,
+        price: 70000,
+        mileage: 60000,
+        fuel: "Flex",
+        brand: "ford",
+        image: "https://picsum.photos/300/200?car=11"
+    },
+    {
+        model: "Chevrolet Tracker",
+        year: 2022,
+        price: 125000,
+        mileage: 15000,
+        fuel: "Flex",
+        brand: "chevrolet",
+        image: "https://picsum.photos/300/200?car=12"
+    },
+    {
+        model: "Volkswagen Virtus",
+        year: 2021,
+        price: 95000,
+        mileage: 25000,
+        fuel: "Gasolina",
+        brand: "volkswagen",
+        image: "https://picsum.photos/300/200?car=13"
+    },
+    {
+        model: "Fiat Cronos",
+        year: 2020,
+        price: 72000,
+        mileage: 30000,
+        fuel: "Flex",
+        brand: "fiat",
+        image: "https://picsum.photos/300/200?car=14"
+    },
+    {
+        model: "Ford Mustang",
+        year: 2022,
+        price: 350000,
+        mileage: 8000,
+        fuel: "Gasolina",
+        brand: "ford",
+        image: "https://picsum.photos/300/200?car=15"
+    },
+    {
+        model: "Chevrolet S10",
+        year: 2021,
+        price: 220000,
+        mileage: 28000,
+        fuel: "Diesel",
+        brand: "chevrolet",
+        image: "https://picsum.photos/300/200?car=16"
+    },
+    {
+        model: "Volkswagen Nivus",
+        year: 2023,
+        price: 140000,
+        mileage: 3000,
+        fuel: "Flex",
+        brand: "volkswagen",
+        image: "https://picsum.photos/300/200?car=17"
+    },
+    {
+        model: "Fiat Ducato",
+        year: 2019,
+        price: 165000,
+        mileage: 55000,
+        fuel: "Diesel",
+        brand: "fiat",
+        image: "https://picsum.photos/300/200?car=18"
+    },
+    {
+        model: "Ford Fusion",
+        year: 2020,
+        price: 115000,
+        mileage: 32000,
+        fuel: "Flex",
+        brand: "ford",
+        image: "https://picsum.photos/300/200?car=19"
+    },
+    {
+        model: "Chevrolet Montana",
+        year: 2022,
+        price: 98000,
+        mileage: 12000,
+        fuel: "Flex",
+        brand: "chevrolet",
+        image: "https://picsum.photos/300/200?car=20"
+    }
+];
